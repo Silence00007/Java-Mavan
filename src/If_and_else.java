e1e78dff29d630a8706af311c18927ecb0f74b43
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-06-17 21:57:49 +0530

    Second commit of the if and else

--- src/If_and_else.java
+++ src/If_and_else.java
@@ -6,7 +6,19 @@ public class If_and_else {
         if , else if and else
         switch case
          */
-
+        int x =99;
+        if (x>10)
+        {
+            System.out.println("statment of the if condition is verified");
+        } else if (x==10) {
+            System.out.println("statment of the else if condition is verified");
+        } else if (x<10 & x>=8) {
+            System.out.println("statment of the else if condition is verified between 8 to 10");
+        }
+        else
+        {
+            System.out.println("all statments are not verified so else condition is verified");
+        }
 
 
     }
