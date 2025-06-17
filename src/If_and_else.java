f8d50d7b1aa81048fcbd46f9e8990de6c52a398c
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-06-17 21:27:18 +0530

    first commit of the if and else

---
+++ src/If_and_else.java
@@ -0,0 +1,13 @@
+public class If_and_else {
+    public static void main(String[] args) {
+        /* this is for the java conditionals statments
+        if
+        if elese
+        if , else if and else
+        switch case
+         */
+
+
+
+    }
+}
