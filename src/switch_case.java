26f0b25a6a8c0a644e42deba5afb74ac8dbdc16b
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-06-17 22:17:47 +0530

    Third commit for the switch case

---
+++ src/switch_case.java
@@ -0,0 +1,25 @@
+;
+
+public class switch_case {
+    public static void main(String[] args) {
+
+        int month_num=12;
+        switch (month_num)
+        {
+            case 1 :
+                System.out.println("January"); break;
+            case 2:
+                System.out.println("February"); break;
+            case 3 :
+                System.out.println("March"); break;
+            case 4 :
+                System.out.println("April"); break;
+            case 5:
+                System.out.println("May"); break;
+            case 6: System.out.println("June"); break;
+            default: System.out.println("Invalid Month");
+
+        }
+
+    }
+}
