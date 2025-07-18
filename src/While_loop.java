b572d4f9e7203c50f771429cbad06aaa5f8fce09
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-07-18 13:06:29 +0530

    i am adding while loop an do while loop

---
+++ src/While_loop.java
@@ -0,0 +1,20 @@
+public class While_loop {
+    public static void main(String[] args) {
+
+        // While loop
+        int x= 100;
+                while(x<=100){
+
+                    System.out.println("Welcome to the while loop ");
+                    x++;
+                }
+
+
+      // do while
+       int y= 10;
+                do{
+                    System.out.println("hello this is the do while loop welcome");
+                    y++;
+                } while (y<=1);
+    }
+}
