43ebcf157268f18a460a3a5960f3edf85cde1271
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-07-21 15:12:24 +0530

    Adding for loop

--- src/While_loop.java
+++ src/While_loop.java
@@ -16,5 +16,17 @@ public class While_loop {
                     System.out.println("hello this is the do while loop welcome");
                     y++;
                 } while (y<=1);
+
+
+
+    // For Loop
+    //Print from 1 to 10
+
+
+    for (int i=1; i<=10; i++)
+    {
+        System.out.println(i);
+    }
+
     }
 }
