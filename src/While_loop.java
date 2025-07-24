583e72bd9a37946c833fa5a9668bd1183dbb6604
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-07-24 13:59:31 +0530

    Add for loop for print 1 to 20 and specify even and odd number

--- src/While_loop.java
+++ src/While_loop.java
@@ -21,12 +21,25 @@ public class While_loop {
 
     // For Loop
     //Print from 1 to 10
-
-
+        System.out.println("Print between 0 to 10 is :");
     for (int i=1; i<=10; i++)
     {
-        System.out.println(i);
+        System.out.println( i);
     }
+
+    // print only even number using foor loop
+        System.out.println("The even and odd number between 0 to 20 is :");
+        for (int e=0; e<=20; e++)
+        {
+            if (e%2==0)
+            {
+                System.out.println("Even Number is " + e);
+            }
+            else
+            {
+                System.out.println("And Odd number is : " + e);
+            }
+        }
 
     }
 }
