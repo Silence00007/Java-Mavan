89c4052eac72ab91dae9c34ab40aafa35a9cbace
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-08-01 17:06:40 +0530

    Adding if else using for the week name

--- src/Java_conditiontions.java
+++ src/Java_conditiontions.java
@@ -10,7 +10,7 @@ public class Java_conditiontions {
         }
         else {
             System.out.println("condition is false so else blocks runs");
-        }*/
+        }
 
 
         // Else IF ,categorise the child, young, adult and senior citizen
@@ -26,7 +26,29 @@ public class Java_conditiontions {
 
         else{
             System.out.println("The Person is belong to the child Age Group");
+        }*/
+
+        // using Else if print the week name
+
+        int week=5;
+        if (week==1){
+            System.out.println("The Name of the Week is MONDAY");
+        } else if (week==2) {
+            System.out.println("The Name of the Week is TUSEDAY");
+        } else if (week==3) {
+            System.out.println("The Name of the Week is WEDNESDAY");
+        } else if (week==4) {
+            System.out.println("The Name of the Week is THURSDAY");
+        } else if (week==5) {
+            System.out.println("The Name of the Week is FRIDAY");
+        } else if (week==6) {
+            System.out.println("The Name of the Week is SATURDAY");
+        } else if (week==7) {
+            System.out.println("The Name of the Week is SUNDAY");
+        } else {
+            System.out.println("INVALID WEEK Please enter the number between 1 to 7");
         }
+
 
     }
 }
