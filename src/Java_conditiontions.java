1077d7d867eeaf461fdd610a57d8d0291f8a36cc
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-08-01 13:39:30 +0530

    Adding if and else

---
+++ src/Java_conditiontions.java
@@ -0,0 +1,19 @@
+public class Java_conditiontions {
+    public  static void main (String [] args)
+    {
+       /* // Java conditions IF and ELSE
+
+        int a = 6;
+        if (a>=10)
+        {
+            System.out.println("we are happy that condition is correct so if blocks run ");
+        }
+        else {
+            System.out.println("condition is false so else blocks runs");
+        }*/
+
+
+        //
+
+    }
+}
