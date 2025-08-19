aee45aaed327e1203c6ada08e32c97fae16b52f3
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-08-19 14:40:22 +0530

    using scanner we use switch case and try to print month of year

--- src/Scanner_usage.java
+++ src/Scanner_usage.java
@@ -21,7 +21,8 @@ public class Scanner_usage {
         }
         else {
             System.out.println("hey welcome to infante age group");
-        }*/
+        }
+        // Print week name by using if else and scanner
 
         System.out.println("Enter the week Number between the number 1 to 7 ");
         //String name = scanner.nextLine();
@@ -43,7 +44,43 @@ public class Scanner_usage {
             System.out.println("The name of the week is  : SUNDAY");
         } else {
             System.out.println("INVALID WEEK NAME");
+        } */
+
+        //print month name by using switch case and scanner
+
+        System.out.println(" Hello !! please enter the number between 1 to 12 !!!!!!....");
+        int month = scanner.nextInt();
+        switch (month)
+        {
+            case 1 :System.out.println("Dear the number of month you feed is JANUARY "); break;
+            case 2 :
+                System.out.println("Dear the number of month you feed is  FEBRUARY"); break;
+            case 3:
+                System.out.println("Dear the number of month you feed is MARCH "); break;
+            case 4 :
+                System.out.println("Dear the number of month you feed is APRIL "); break;
+            case 5 :
+                System.out.println("Dear the number of month you feed is MAY"); break;
+            case 6 :
+                System.out.println("Dear the number of month you feed is JUNE"); break;
+            case 7:
+                System.out.println("Dear the number of month you feed is JULY"); break;
+            case 8 :
+                System.out.println("Dear the number of month you feed is AUGUST"); break;
+            case 9 :
+                System.out.println("Dear the number of month you feed is SEPTEMBER"); break;
+            case 10 :
+                System.out.println("Dear the number of month you feed is OCTOBER"); break;
+            case 11 :
+                System.out.println("Dear the number of month you feed is NOVEMBER "); break;
+            case 12 :
+                System.out.println("Dear the number of month you feed is 'DECEMBER' "); break;
+
+            default:
+                System.out.println("This as an Invalid month please enter the number between 1 to 12");
+
         }
+
 
     }
 
